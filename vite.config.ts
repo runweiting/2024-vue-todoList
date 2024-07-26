922fb86e4279ccc39fa4c45105c048785da167be
runweiting <whatisfuntime@gmail.com>
2024-07-26 10:51:27 +0800

    docs: ✏️ 新增 base

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  // 要和 GitHub Repo 名稱一致
+  base: '/Composition-API-todoList/',
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
